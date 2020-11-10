04f049b7ea988969e77e4c452d18e2a59407280a
Xinhao Zhang <xinhao-zhang@teletracking.com>
2020-11-10 11:48:15 -0600

    add filesearch

---
+++ filesearch/ConcurrentFileSearch.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	matches   []string
+	waitgroup = sync.WaitGroup{}
+	lock      = sync.Mutex{}
+)
+
+func fileSearch(root string, filename string) {
+	fmt.Println("Searching in", root)
+	files, _ := ioutil.ReadDir(root)
+	for _, file := range files {
+		if strings.Contains(file.Name(), filename) {
+			lock.Lock()
+			matches = append(matches, filepath.Join(root, file.Name()))
+			lock.Unlock()
+		}
+		if file.IsDir() {
+			waitgroup.Add(1)
+			go fileSearch(filepath.Join(root, file.Name()), filename)
+		}
+	}
+	waitgroup.Done()
+}
+
+func main() {
+	t1 := time.Now()
+	waitgroup.Add(1)
+	go fileSearch("/", "README.md")
+	waitgroup.Wait()
+	for _, file := range matches {
+		fmt.Println("Matched", file)
+	}
+	elapsed := time.Since(t1)
+	fmt.Println("App elapsed: ", elapsed)
+}
